88d14a4bd53b6bd7952289017b673b5fdb6b96ad
TaehoonKim <lazygame05@gmail.com>
2019-04-21 17:25:11 -0400

    Chat html and js

---
+++ fireinit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAv7DQXJCKiX8RpGbtWP_hhtLvprowK9OU",
+  authDomain: "mafia-game-bd3c8.firebaseapp.com",
+  databaseURL: "https://mafia-game-bd3c8.firebaseio.com",
+  projectId: "mafia-game-bd3c8",
+  storageBucket: "mafia-game-bd3c8.appspot.com",
+  messagingSenderId: "330855808685"
+};
+firebase.initializeApp(config);

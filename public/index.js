16b8c24f1305cf8024f1cd6821e1420a400d2ea4
Kate Yukyung Lee <sddmac@pal-nat186-76-73.itap.purdue.edu>
2019-04-21 16:44:20 -0400

    google auth log in

---
+++ public/index.js
@@ -0,0 +1,64 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+
+
+function login(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+     var token = result.credential.accessToken;
+     var user = result.user;
+   
+     console.log(token)
+     console.log(user)
+  }).catch(function(error) {
+     var errorCode = error.code;
+     var errorMessage = error.message;
+   
+     console.log(error.code)
+     console.log(error.message)
+  });
+
+  // firebase.auth().signInWithRedirect(provider);
+
+  // var userEmail = document.getElementById("email_field").value;
+  // var userPass = document.getElementById("password_field").value;
+
+  // firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+
+  //   window.alert("Error : " + errorMessage);
+
+  //   // ...
+  // });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}

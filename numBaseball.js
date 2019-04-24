4bfb804586661cd980290cb894dfe2a2c39ca401
TaehoonHoodyKim <40323560+TaehoonHoodyKim@users.noreply.github.com>
2019-04-23 22:18:30 -0400

    Fix the Ball point bug

--- numBaseball.js
+++ numBaseball.js
@@ -1,13 +1,20 @@
 var body = document.body;
 
 //list of the possible number
-var num_arr = [1,2,3,4,5,6,7,8,9];
-var ans_arr = []; // this array will have 4 random number from the list.
+var num_arr; 
 
-for(var i=0; i< 4; i+=1){
-    var sele = num_arr.splice(Math.floor(Math.random()*(9 - i)),1)[0];
-    ans_arr.push(sele);
+var ans_arr;
+
+//make the number_pick as a function to make code short.
+function pickNum(){
+    num_arr= [1,2,3,4,5,6,7,8,9];
+    ans_arr= []; // this array will have 4 random number from the list.
+    for(var i=0; i< 4; i+=1){
+        var sele = num_arr.splice(Math.floor(Math.random()*(9 - i)),1)[0];
+        ans_arr.push(sele);
+    }
 }
+pickNum();
 console.log(ans_arr);
 //its for result
 var w_result = document.createElement('h1');
@@ -40,8 +47,9 @@ w_form.addEventListener('submit', function call_back(event){
     console.log(ans,ans_arr,ans==ans_arr.join(''));
     if( ans == ans_arr.join('')){
         w_result.textContent='HomeRun!!! you tried'+w_tryNum+' times'
-        text_input = '';
-        
+        text_input.value='';
+        pickNum();
+        w_tryNum=0;
     }else{
         // if user didn't get the answer, then 
         // show the hints to the user.
@@ -54,7 +62,7 @@ w_form.addEventListener('submit', function call_back(event){
                 value of the strike, ball  for making hints.
         */
        console.log('wrong answer!', user_ansArr);
-        for( var i = 0 ; i< 3; i+=1){
+        for( var i = 0 ; i< 4; i+=1){
 
             if(Number(user_ansArr[i]) == ans_arr[i]){
                 strike +=1;
@@ -63,7 +71,9 @@ w_form.addEventListener('submit', function call_back(event){
             }else{
                 out+=1;
             }
-            w_result.textContent = strike+'strike & '+ball + 'ball'
+            w_result.textContent = strike+'strike & '+ball + 'ball ' + out +' out';
+            text_input.value='';
+            text_input.focus();
         }
     }
 });

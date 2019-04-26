8aa0b35105c1f2da322802029a6ef15cfbfae5f7
choijay12 <33638506+choijay12@users.noreply.github.com>
2019-04-26 00:45:52 -0400

    yeaaa

--- MafiaGame_cs252/numBaseball.js
+++ MafiaGame_cs252/numBaseball.js
@@ -17,29 +17,90 @@ function pickNum(){
 pickNum();
 console.log(ans_arr);
 //its for result
-var w_result = document.createElement('h1');
-body.append(w_result);
+var w_result = document.getElementById('resultHeader');
+//body.append(w_result);
 //create the form of it.
-var w_form = document.createElement('form');
-document.body.append(w_form);
+var w_form = document.getElementById('form1');
+//document.body.append(w_form);
 //about the textbox to input the number
-var text_input = document.createElement('input');
-text_input.type='text';
-text_input.maxLength=4;  // set the max entered number -> 4
-//add the text box.
-w_form.append(text_input);
+//var text_input = document.getElementById('ans');
 
+//text_input.type='text';
+//text_input.maxLength=4;  // set the max entered number -> 4
+//add the text box.
+//w_form.append(text_input);
+/*
 //add the button 
-var w_button = document.createElement('button');
+
 //enter the text inside the button.
 w_button.textContent = 'Enter';
 
 w_form.append(w_button);
-
+*/
+var w_button = document.getElementById('yas');
 var w_tryNum =0;
+var w_image = document.getElementById('homerunImage');
+var flag = 0;
+
+function checkAns(){
+    var text_input = document.getElementById('ans');
+    
+
+    w_tryNum+=1;
+    
+    var ans = text_input.value;
+    console.log("try time: " + w_tryNum)
+    console.log(ans,ans_arr,ans==ans_arr.join(''));
+    if( ans == ans_arr.join('')){
+        w_result.textContent='Home Run!!! Your attempts : ' + w_tryNum; // Fix1
+        // w_image.src = "homerun.jpg";
+        flag = 1;
+        text_input.value='';
+        text_input.focus();
+        pickNum();
+        w_tryNum=0;
+    }else{
+        
+        var user_ansArr = ans.split('');
+        var strike =0;
+        var ball =0;
+        var out =0;
+        
+        console.log('wrong answer!', user_ansArr);
+        if(w_tryNum>10){
+            w_result.textContent='You failed over 10 times! answer is : '+ans_arr.join(',');
+            w_trynum =0;
+            
+            pickNum();
+
+        }else{
+            for( var i = 0 ; i< 4; i+=1){
+
+                if(Number(user_ansArr[i]) == ans_arr[i]){
+                    strike +=1;
+                }else if(ans_arr.indexOf(Number(user_ansArr[i]))>-1){
+                    ball+=1;
+                }else{
+                    out+=1;
+                }
+                w_result.textContent = strike+' strike & '+ball + ' ball & ' + out +' out';
+                text_input.value='';
+                text_input.focus();
+            }
+        }
+        
+    }
+    w_image.src = "homerun.jpg";
+    text_input = document.getElementById('ans');
+
+    //you tried over 10 times. Please enter anyting 
+    // to play again.
+    
+}
 
 // add the event & event.preventDefault() = > prevent the reset all things.
 // 'submit' -> default works is refresh the page.
+/*
 w_form.addEventListener('submit', function call_back(event){
     w_tryNum+=1;
     event.preventDefault();
@@ -59,10 +120,7 @@ w_form.addEventListener('submit', function call_back(event){
         var strike =0;
         var ball =0;
         var out =0;
-        /*
-                user_ansArr & ans_arr compare and increase the 
-                value of the strike, ball  for making hints.
-        */
+      
        console.log('wrong answer!', user_ansArr);
        if(w_tryNum>10){
             w_result.textContent='You failed over 10 times! answer is : '+ans_arr.join(',');
@@ -89,6 +147,6 @@ w_form.addEventListener('submit', function call_back(event){
         
     }
 });
-
+*/
 
 
